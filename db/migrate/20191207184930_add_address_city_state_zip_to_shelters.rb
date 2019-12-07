5aa98ba24c59b106fc759345063dc9a3d2157ece
Kevin Krato <kratok08@gmail.com>
2019-12-07 11:56:03 -0700

    Generate migrations to DB for extra shelter columns

---
+++ db/migrate/20191207184930_add_address_city_state_zip_to_shelters.rb
@@ -0,0 +1,8 @@
+class AddAddressCityStateZipToShelters < ActiveRecord::Migration[6.0]
+  def change
+    add_column :shelters, :address, :string
+    add_column :shelters, :city, :string
+    add_column :shelters, :state, :string
+    add_column :shelters, :zip, :integer
+  end
+end
