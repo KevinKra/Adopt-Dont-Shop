a81c60f53e23074bc002064ae32963ba81793d41
Kevin Krato <kratok08@gmail.com>
2019-12-07 15:42:04 -0700

    Add seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+shelter_1 = Shelter.create(
+  title: "Big Woof",
+  city: "Denver",
+  state: "CO",
+  address: "515 Clarke Rd.",
+  zip: 32081
+ )
+shelter_2 = Shelter.create(
+  title: "Hounds Home",
+  city: "Chicago",
+  state: "IL",
+  address: "4147 18th St.",
+  zip: 43088
+ )
