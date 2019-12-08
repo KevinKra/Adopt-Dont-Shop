da202dcb175769e90effd58485215b10a8c6d680
Kevin Krato <kratok08@gmail.com>
2019-12-08 13:56:18 -0700

    Update shelterReviews controller with new and create actions

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -1,5 +1,16 @@
 class ShelterReviewsController < ApplicationController
   def new 
-    
+    @shelter = Shelter.find(params[:shelter_id])
   end
+
+  def create
+    shelter = Shelter.find(params[:id])
+    shelter.shelterReviews.create(shelter_review_params)
+    redirect_to "/shelters/#{shelter.id}"
+  end
+
+  private
+    def shelter_review_params
+      params.permit(:title, :rating, :review, :photo)
+    end
 end
