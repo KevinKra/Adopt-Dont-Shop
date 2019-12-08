fb0d66a121de147cb6fcc6e21fe4bd82dd8085b8
Kevin Krato <kratok08@gmail.com>
2019-12-07 19:42:20 -0700

    Add routes, controller actions, views, for pet creation per shelter

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,20 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @shelter = Shelter.find(params[:shelter_id])
+  end
+
+  def create
+    # binding.pry
+    shelter = Shelter.find(params[:shelter_id])
+    shelter.pets.create(pet_params)
+    redirect_to "/shelters/#{shelter.id}/pets"
+  end
+
+  private
+  def pet_params
+    params.permit(:name, :age, :image, :description, :age, :sex, :adoptable)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   get "/shelters/new", to: "shelters#new"
   get "/shelters/:id", to: "shelters#show"
   get "/shelters/:id/edit", to: "shelters#edit"
-  get "/shelters/:shelter_id/pets", to: "shelters#index_pets"
+
   post "/shelters", to: "shelters#create"
   patch "/shelters/:id", to: "shelters#update"
   delete "/shelters/:id", to: "shelters#destroy"
+
+  # shelter-pets
+  get "/shelters/:shelter_id/pets", to: "shelters#index_pets"
+  get "/shelters/:shelter_id/pets/new", to: "pets#new"
+  post "/shelters/:shelter_id/pets", to: "pets#create"
 
   # pets
   get "/pets", to: "pets#index"
