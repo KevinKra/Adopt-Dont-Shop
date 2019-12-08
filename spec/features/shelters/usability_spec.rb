95d40bae8f3b3b057962159265792e9202f549d6
Kevin Krato <kratok08@gmail.com>
2019-12-08 15:36:22 -0700

    Add and test additional shelter usability features

--- spec/features/shelters/usability_spec.rb
+++ spec/features/shelters/usability_spec.rb
@@ -56,6 +56,14 @@ RSpec.describe "Usability features for shelter's index page" do
     expect(page).to_not have_content("BarkTown Dogs")
   end
 
+  it "should allow user to visit the shelter's show page from /shelters" do
+    visit "/shelters"
+
+    click_link "#{@shelter_1.title}"
+
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}")
+  end
+
   it "should allow user to visit the shelter's pets index page from /shelters/:id" do
     visit "/shelters/#{@shelter_1.id}"
 
